345cdce9d488ee9914c4629bef7cb5b35fdbe54e
danyel-dev <danyelpinheiro154@hotmail.com>
2022-10-27 11:37:28 -0300

    Componente de lista criada

---
+++ src/components/ListComponent.jsx
@@ -0,0 +1,13 @@
+export default function ListComponent() {
+    return (
+        <div>
+            <p>Lista 01</p>
+            
+            <ul>
+                <li>Item 01</li>
+                <li>Item 02</li>
+                <li>Item 03</li>
+            </ul>
+        </div>
+    );
+}
---
+++ src/pages/Home.jsx
@@ -0,0 +1,10 @@
+import ListComponent from "../components/ListComponent";
+
+
+export default function Home() {
+    return (
+        <div>
+            <ListComponent></ListComponent>
+        </div>
+    );
+}
