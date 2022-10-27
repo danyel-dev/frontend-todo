a1f39efd641dbb370a2488717d3a9f488e33984a
danyel-dev <danyelpinheiro154@hotmail.com>
2022-10-27 11:56:27 -0300

    Componente para visualizar os Items criado

---
+++ src/components/ItemComponent.jsx
@@ -0,0 +1,5 @@
+export default function ItemComponent() {
+    return (
+        <li>Item 01</li>
+    );
+}
--- src/components/ListComponent.jsx
+++ src/components/ListComponent.jsx
@@ -1,12 +1,13 @@
+import ItemComponent from "./ItemComponent";
+
+
 export default function ListComponent() {
     return (
         <div>
             <p>Lista 01</p>
-            
+
             <ul>
-                <li>Item 01</li>
-                <li>Item 02</li>
-                <li>Item 03</li>
+                <ItemComponent></ItemComponent>
             </ul>
         </div>
     );
